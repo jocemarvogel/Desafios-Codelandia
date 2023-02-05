ffb81f6e17543b32de95aea73c4f8dd185f6fb31
Jocemar vogel <102194692+jocemarvogel@users.noreply.github.com>
2023-02-05 10:26:29 -0300

    adicionando script

---
+++ Front End/Loja de Tenis/script.js
@@ -0,0 +1,18 @@
+/* Type */
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30
+    });
+
+    /* Scroll Reveal */
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '50px',
+        duration: 2000,
+        reset: true
+    });
+
+    ScrollReveal().reveal('.titulo', { delay: 50 });
+    ScrollReveal().reveal('.textShop', { delay: 50 });
+    ScrollReveal().reveal('.apresentacao', { delay: 50 });
+    ScrollReveal().reveal('.imgTenis', { delay: 50 });
